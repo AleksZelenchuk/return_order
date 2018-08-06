352ce9d0b1ea6a8cbca400fffe6c01a79a7ab9f5
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-08-06 13:52:05 +0300

    Update RequiredOptions.php

--- ReturnOrder/Ui/Component/Listing/Grid/Column/RequiredOptions.php
+++ ReturnOrder/Ui/Component/Listing/Grid/Column/RequiredOptions.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Fantronix\ReturnOrder\Ui\Component\Listing\Grid\Column;
+namespace Namespace\ReturnOrder\Ui\Component\Listing\Grid\Column;
 
 class RequiredOptions implements \Magento\Framework\Option\ArrayInterface
 {
