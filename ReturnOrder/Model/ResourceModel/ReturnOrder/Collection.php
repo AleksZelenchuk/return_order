377ee3914a415e5a73247e0d7c7d837b1fb24de5
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-08-06 13:53:37 +0300

    Update Collection.php

--- ReturnOrder/Model/ResourceModel/ReturnOrder/Collection.php
+++ ReturnOrder/Model/ResourceModel/ReturnOrder/Collection.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- *
- * @Author              Ngo Quang Cuong <bestearnmoney87@gmail.com>
- * @Date                2016-12-16 02:04:31
- * @Last modified by:   nquangcuong
- * @Last Modified time: 2016-12-24 17:57:36
- */
-
-namespace Fantronix\ReturnOrder\Model\ResourceModel\ReturnOrder;
+namespace Namespace\ReturnOrder\Model\ResourceModel\ReturnOrder;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
@@ -22,6 +14,6 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
      */
     protected function _construct()
     {
-        $this->_init('Fantronix\ReturnOrder\Model\ReturnOrder', 'Fantronix\ReturnOrder\Model\ResourceModel\ReturnOrder');
+        $this->_init('Namespace\ReturnOrder\Model\ReturnOrder', 'Namespace\ReturnOrder\Model\ResourceModel\ReturnOrder');
     }
 }
